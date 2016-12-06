c436d21ddab03737d9cde67e83f2af279c9467c4
aggresss <aggresss@163.com>
2016-12-06 16:44:42 +0800

    version 20161206C

---
+++ Code/hello_Execute/Android.mk
@@ -0,0 +1,11 @@
+ LOCAL_PATH := $(call my-dir)
+
+      include $(CLEAR_VARS)
+
+      LOCAL_MODULE_TAGS := optional
+
+      LOCAL_MODULE := hello
+
+      LOCAL_SRC_FILES := $(call all-subdir-c-files)
+
+      include $(BUILD_EXECUTABLE)
